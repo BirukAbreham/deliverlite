7bf58a9d31a01b7780724d4e32334b9eb895751e
BirukAbreham <birukabreham0@gmail.com>
2020-12-18 07:09:03 +0300

    Completed contact, group and campaign main end points routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts, only: [ :index, :show, :create, :update, :destroy ] do
+    collection do
+      get 'search', to: 'contacts#search'
+    end
+    member do
+      get 'activities', to: 'contacts#activity'
+      get 'groups', to: 'contacts#groups'
+    end
+  end
+
+  resources :groups, only: [ :index, :show, :create, :update, :destroy ] do
+    member do
+      get 'contacts', to: 'groups#contacts'
+      get 'campaigns', to: 'groups#campaigns'
+    end
+  end
+
+  resources :campaigns, only: [ :index, :show, :create, :update, :destroy ] do
+    member do
+      get 'activities', to: 'campaings#activity'
+      get 'groups', to: 'campaings#groups'
+    end
+  end
 end
